d1bf1735eecaeed0475ee6998f53b9da4035f9ce
Gabriel Titato <71743284+gabrieltitato@users.noreply.github.com>
2024-11-18 19:40:59 -0300

    feat: add project to github repository

---
+++ src/main/java/com/kipper/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.kipper.email_service.application;
+
+import com.kipper.email_service.core.EmailSenderUseCase;
+import com.kipper.email_service.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService (EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
---
+++ src/main/java/com/kipper/email_service/infra/ses/SesEmailSender.java
@@ -0,0 +1,36 @@
+package com.kipper.email_service.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.kipper.email_service.adapters.EmailSenderGateway;
+import com.kipper.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("enobregabriel@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+        try{
+            this.amazonSimpleEmailService.sendEmail(request);
+        }catch (AmazonServiceException exception){
+            throw new EmailServiceException("Failure while sending email", exception);
+        }
+    }
+}
